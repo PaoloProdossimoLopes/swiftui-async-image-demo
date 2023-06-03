2af1156cc6bc1ffc0cf503379d76ac2af30d431b
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-06-03 02:39:32 -0300

    Initial Commit

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Paolo Prodossimo Lopes on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
